88e5163a3919969927e02fe9490fbc4a3b2d7aa6
shafay <shafayboost@outlook.com>
2024-09-09 21:31:01 +0500

    at last changes done .

--- src/app/api/auth/[...nextauth]/AuthOptions.tsx
+++ src/app/api/auth/[...nextauth]/AuthOptions.tsx
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../libs/prismadb";
 import bcrypt from "bcrypt";
 
 export const authOptions: AuthOptions = {
